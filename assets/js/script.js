8baaf3033758a09d21518abc3fa87d6f0c470330
yasui05821 <yasui05821@gmail.com>
2017-05-09 14:31:29 +0900

    issue #96対応　スライド実装　途中

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,22 @@ $(function(){
         speed: 300
     });
 });
+
+
+// Slick Slide
+// TODO FIX CLASS NAME
+$(function(){
+    $('.item_visual').slick({
+        dots: true,
+        arrows: false,
+    });
+});
+$(function(){
+    $('.item_nav').slick({//サムネイル画像
+        dots: false,
+        arrows:false,
+        slidesToShow: 3,
+        focusOnSelect: true,
+        asNavFor: '.item_visual',//スライダー部分の要素を記述
+    });
+});
