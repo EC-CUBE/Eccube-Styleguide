0b785a52d48c4cc163466c976d36a5e5da0351a3
yasui05821 <yasui05821@gmail.com>
2017-06-05 12:48:12 +0900

    topへ戻る　js実装

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,13 @@ $(function(){
     })
 })
 
+$(function(){
+    $(".ec-blockTopBtn").on("click",function(){
+        $("html,body").animate({"scrollTop":0},500);
+    })
+})
+
+
 // Slick Slide
 // TODO FIX CLASS NAME
 $(function(){
